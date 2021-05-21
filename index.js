63102d5f93795511889579df9051a8ce0dbfed11
Chinecherem Ubawike <61988607+Neche-Stephen@users.noreply.github.com>
2021-05-21 08:08:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
-// function convertFahrToCelsius(Fahr){
-//     if(isNaN(Fahr)){
-//         console.log(`${Fahr} is not a valid number but a/an ${typeof(Fahr)}.`);
-//      }else{
-//         Celsius = (Fahr - 32) * 5/9;
-//         Celsius = Celsius.toFixed(4)
-//         console.log(Celsius);
-//      }
+// Task 1
+function convertFahrToCelsius(Fahr){
+    if(isNaN(Fahr)){
+        console.log(`${Fahr} is not a valid number but a/an ${typeof(Fahr)}.`);
+     }else{
+        Celsius = (Fahr - 32) * 5/9;
+        Celsius = Celsius.toFixed(4)
+        console.log(Celsius);
+     }
    
 
-// }
-// convertFahrToCelsius({temp: 0})
+}
+convertFahrToCelsius(0)
 
-
+// Task 2
 function checkYuGiOh(n){
+   if(isNaN(n)){
+      console.log(`${n} is not a valid number but a/an ${typeof(n)}.`) 
+    }
    const arr = Array.from({length: n}, (_, index) => index + 1);
-   console.log(arr);
+   for (i = 0; i < arr.length; i++) {
+      if( (arr[i] % 2 == 0 ) && (arr[i] % 3 != 0) &&  ( arr[i] % 5 != 0 )){
+         console.log("Yu")
+      }
+
+      else if( (arr[i] % 3 == 0 ) && (arr[i] % 2 != 0) &&  ( arr[i] % 5 != 0 )){
+      console.log("Gi")
+      }
+
+     else if( (arr[i] % 5 == 0 ) && (arr[i] % 2 != 0) &&  ( arr[i] % 5 != 0 )){
+      console.log("Oh")
+     }
+
+     else if(  (arr[i] % 2 == 0)  &&  (arr[i] % 3 == 0) &&  ( arr[i] % 5 != 0 ) ){
+         console.log('Yu-Gi')
+     }  
+
+     else if(  (arr[i] % 2 == 0)  &&  (arr[i] % 5 == 0) &&  ( arr[i] % 3 != 0 ) ){
+      console.log('Yu-Oh')
+     }
+     else if(  (arr[i] % 3 == 0)  &&  (arr[i] % 5 == 0) &&  ( arr[i] % 2 != 0 ) ){
+     console.log('Gi-Oh')
+     }
+     else if( ( arr[i] % 2 == 0 ) && ( arr[i] % 3 == 0 ) && ( arr[i] % 5 == 0 ) ){
+      console.log('Yu-Gi-Oh')
+     }                             
+     else if( ( arr[i] % 2 != 0 ) && ( arr[i] % 3 != 0 ) && ( arr[i] % 5 != 0 ) ){
+     console.log(arr[i])
+     }
+    
+   }
+   
 
 }
-checkYuGiOh(5)
-// Write a function named "checkYuGiOh" that takes a number, n, as an argument, creates an array of numbers from 1 to n and replaces multiples of 2, 3, and 5 with "yu", "gi" and "oh", then returns the resulting array.
-
-// Note:
-
-// - for numbers that have multiple factors, use hyphens as separators
-// e.g 10 === "yu-oh", 30 === "yu-gi-oh"
-// - perform checks on your input and return `invalid parameter: ${parameter}` if an invalid parameter - i.e. a string that can't be converted to a number or another data type - is passed. 
-// Use the following cases as guides:
-
-// - checkYuGiOh(10) should return [1, "yu", "gi", "yu", "oh", "yu-gi", 7, "yu", "gi", "yu-oh"]
-// - checkYuGiOh("5") should return [1, "yu", "gi", "yu", "oh"]
-// - checkYuGiOh("fizzbuzz is meh") should return `invalid parameter: "fizzbuzz is meh"`
-
-// if the parameter passed is not a number or a string that can be converted to a valid number, return a string with the format below:
-//     `${parameter} is not a valid number but a/an ${parameter's type}.`
+checkYuGiOh(10)
